fde052da0a12ae3a2e1bb08e2ebdc5b3bc714e56
Awab <awabalshandawi@gmail.com>
2025-12-15 08:53:10 +0800

    so far so good

--- server/server.js
+++ server/server.js
@@ -4,45 +4,50 @@ const fs = require("fs");
 const path = require("path");
 
 const httpServer = createServer((req, res) => {
-  if (req.url === "/") {
-    const filePath = path.join(__dirname, "index.html");
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.end("File not found");
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/index.js") {
-    const filePath = path.join(__dirname, "index.js");
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.end("File not found");
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "application/javascript" });
-      res.end(data);
-    });
-  }
+  // Serve client files
+  let filePath =
+    req.url === "/"
+      ? path.join(__dirname, "../client/index.html")
+      : path.join(__dirname, "../client", req.url);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not found");
+      return;
+    }
+
+    const ext = path.extname(filePath);
+    const contentType =
+      ext === ".js" ? "application/javascript" : "text/html";
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  });
 });
 
-const io = new Server(httpServer, { cors: { origin: "*" } });
+const io = new Server(httpServer, {
+  cors: { origin: "*" }
+});
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  // Send message to the connected client
-  socket.emit("message", "Hello from server!");
+  // Send a message to the client upon connection(it will show in the browser console)
+  socket.emit("message", "Hello from server");
+
+  socket.emit("message", "Hello from server123");
+
+  socket.on("message", (data) => {
+    console.log("From client:", data);
+  });
 
   // Handle disconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
   });
 });
-// Start the server
+
 httpServer.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server running at http://localhost:3000");
 });
