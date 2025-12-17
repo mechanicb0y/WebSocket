efc285b07a758a1f77998c1c75b9f8b1a483e1f1
Awab <awabalshandawi@gmail.com>
2025-12-17 11:08:57 +0800

    Now We Are GooD

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,21 +3,36 @@ import io from 'socket.io-client'
 import './App.css'
 
 function App() {
-  const socket = io('localhost:3000')
-
-  function connectSocket() {
-    socket.on('connection', (socket) => {
-      console.log(socket)
-    });
-
-  }
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
+    // Create socket connection
+    const newSocket = io('http://localhost:3000');
+    setSocket(newSocket);
 
-  }, [])
+    // Socket event listeners
+    newSocket.on('connect', () => {
+      console.log('Connected to server:', newSocket.id);
+      setIsConnected(true);
+    });
+
+    newSocket.on('disconnect', () => {
+      console.log('Disconnected from server');
+      setIsConnected(false);
+    });
+
+    // Cleanup on unmount
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <h1>React Multiplayer Dashboard</h1>
+      <p>Connection status: {isConnected ? 'Connected' : 'Disconnected'}</p>
+      <p>Socket ID: {socket?.id || 'Not connected'}</p>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,59 +1,25 @@
+
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-// const fs = require("fs");
-// const path = require("path");
 
-const httpServer = createServer();
+const httpServer = createServer(); // Consistent naming
 
-const socket = new Server(httpServer, {
+const io = new Server(httpServer, {
   cors: {
     origin: "http://localhost:5173",
+    credentials: true,
   },
 });
-// Serve client files
-//   let filePath =
-//     req.url === "/"
-//       ? path.join(__dirname, "../client/index.html")
-//       : path.join(__dirname, "../client", req.url);
 
-//   fs.readFile(filePath, (err, data) => {
-//     if (err) {
-//       res.writeHead(404);
-//       res.end("Not found");
-//       return;
-//     }
+io.on("connection", (socket) => {
+  console.log("socket connected:", socket.id);
 
-//     const ext = path.extname(filePath);
-//     const contentType =
-//       ext === ".js" ? "application/javascript" : "text/html";
-
-//     res.writeHead(200, { "Content-Type": contentType });
-//     res.end(data);
-//   });
-// });
-
-// const io = new Server(httpServer, {
-//   cors: { origin: "*" }
-// });
-
-socket.on("connection", (socket) => {
-  console.log("socket");
-
+  socket.on("message", (data) => {
+    console.log("Message from client:", data);
+  });
 });
-// هنا الي بيظهر في الكوسنول في الويب سايد
-// Send a message to the client upon connection(it will show in the browser console)
-
-// socket.emit("message", "Hello from server123");
-
-// socket.on("message", (data) => {
-//   console.log("From client:", data);
-// });
-
-// Handle disconnection
-// socket.on("disconnect", () => {
-//   console.log("Client disconnected:", socket.id);
-// });
 
 httpServer.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
+
